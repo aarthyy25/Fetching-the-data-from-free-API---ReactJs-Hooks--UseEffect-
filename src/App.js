2a2d3d2bed69c80cbaed4209fa04cff576d19b33
Aarthi R <166208418+aarthyy25@users.noreply.github.com>
2024-08-28 10:52:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,64 +1,49 @@
-// import logo from './logo.svg';
-// import './App.css';
+import ReactDOM from 'react-dom';
+import React, {useState, useEffect} from 'react';
+import './App.css';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <div class="menu">
-//           <ol>
-//             <li>Learn</li>
-//             <li>Reference</li>
-//             <li>Study Material</li>
-//             <li><button>Login</button></li>
-//           </ol>
-//         </div>
-//         <table>
-//           <tr>
-//             <th>Topic Name</th>
-//             <th>Start Date</th>
-//             <th>End Date</th>
-//           </tr>
-//           <tr>
-//             <td>Components</td>
-//             <td>29-07-2024</td>
-//             <td>29-07-2024</td>
-//           </tr>
-//           <tr>
-//             <td>Properties</td>
-//             <td>30-07-2024</td>
-//             <td>20-07-2024</td>
-//           </tr>
-//         </table>
-//       </header>
-//     </div>
-//   );
-// }
 
-// // export default App;
 
-// function Welcome(){
-//   return(
-//    <div>
-//     <p>enfjeruugflewfuhreoipoieufiroegjfbvfdvsda;nkdvn fjdbveyrgiuehv uergejmrhagoerujgjpoedcfkapoejfiaebcn uesraveof8rgweie8034y854367uhdrewubdchafulesfhksfei
-//       hbhefjbuawluopwei[qosla,ds,mdjbeyfrhuerjoijgfldsvkjfdsbiog jioifjioewufjiefjoerwisfjowafcjreeosijereoijsndjfn ,ds].
-//     </p>
-//     <p>enfjeruugflewfuhreoipoieufiroegjfbvfdvsda;nkdvn fjdbveyrgiuehv uergejmrhagoerujgjpoedcfkapoejfiaebcn uesraveof8rgweie8034y854367uhdrewubdchafulesfhksfei
-//       hbhefjbuawluopwei[qosla,ds,mdjbeyfrhuerjoijgfldsvkjfdsbiog jioifjioewufjiefjoerwisfjowafcjreeosijereoijsndjfn ,ds].
-//     </p>
-//     <p>enfjeruugflewfuhreoipoieufiroegjfbvfdvsda;nkdvn fjdbveyrgiuehv uergejmrhagoerujgjpoedcfkapoejfiaebcn uesraveof8rgweie8034y854367uhdrewubdchafulesfhksfei
-//       hbhefjbuawluopwei[qosla,ds,mdjbeyfrhuerjoijgfldsvkjfdsbiog jioifjioewufjiefjoerwisfjowafcjreeosijereoijsndjfn ,ds].
-//     </p>
-//     <p>enfjeruugflewfuhreoipoieufiroegjfbvfdvsda;nkdvn fjdbveyrgiuehv uergejmrhagoerujgjpoedcfkapoejfiaebcn uesraveof8rgweie8034y854367uhdrewubdchafulesfhksfei
-//       hbhefjbuawluopwei[qosla,ds,mdjbeyfrhuerjoijgfldsvkjfdsbiog jioifjioewufjiefjoerwisfjowafcjreeosijereoijsndjfn ,ds].
-//     </p>
-//     <p>enfjeruugflewfuhreoipoieufiroegjfbvfdvsda;nkdvn fjdbveyrgiuehv uergejmrhagoerujgjpoedcfkapoejfiaebcn uesraveof8rgweie8034y854367uhdrewubdchafulesfhksfei
-//       hbhefjbuawluopwei[qosla,ds,mdjbeyfrhuerjoijgfldsvkjfdsbiog jioifjioewufjiefjoerwisfjowafcjreeosijereoijsndjfn ,ds].
-//     </p>
-//    </div>
-//   )
-// }
+function Welcome(){
+  const[users,setUsers]=useState([]);
+  const[loading, settLoading]=useState(true);
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(data =>{
+       setUsers(data);
+       settLoading(false);
+    })
+    .catch(error=>{
+      console.error("If it is not loading, will be error msg");
+      settLoading(false);
+    });
+  },[]);
 
-// export default Welcome;
+    if(loading){
+      return <p>Loading</p>;
+
+    }
+    return(
+      <div>
+        <h1>List out the users in API</h1>
+        <table>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>UserName</th>
+          </tr>
+          {users.map(user =>(
+            <tr>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.username}</td>
+            </tr>
+          ))}
+        </table>
+      </div>
+  )
+
+}
 
 
