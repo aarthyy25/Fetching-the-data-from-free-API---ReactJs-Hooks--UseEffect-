211a2d9c5ab91ed21a176a1c57c0e57b1953b397
Aarthi R <166208418+aarthyy25@users.noreply.github.com>
2024-08-28 10:55:48 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,50 +1,11 @@
 import ReactDOM from 'react-dom';
-import React, {useState, useEffect} from 'react';
-import './App.css';
-
-
+import App from './App';
 
 function Welcome(){
-  const[users,setUsers]=useState([]);
-  const[loading, settLoading]=useState(true);
-  useEffect(()=>{
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(data =>{
-       setUsers(data);
-       settLoading(false);
-    })
-    .catch(error=>{
-      console.error("If it is not loading, will be error msg");
-      settLoading(false);
-    });
-  },[]);
-
-    if(loading){
-      return <p>Loading</p>;
-
-    }
-    return(
-      <div>
-        <h1>List out the users in API</h1>
-        <table>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>UserName</th>
-          </tr>
-          {users.map(user =>(
-            <tr>
-              <td>{user.id}</td>
-              <td>{user.name}</td>
-              <td>{user.username}</td>
-            </tr>
-          ))}
-        </table>
-      </div>
+  return(
+    <App />
   )
-
 }
-ReactDOM.render(<Welcome />, document.getElementById('root'));  // This will render the Welcome component to the
+ReactDOM.render(<Welcome />, document.getElementById('root')); 
 
 
